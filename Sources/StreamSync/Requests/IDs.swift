c433d0dba0f5c597e4fe32d0587b827f621222a9
Loki <sergiy@vynnychenko.com>
2022-07-10 19:41:07 +0300

    Stream 2.0 wip

--- Sources/StreamSync/Requests/IDs.swift
+++ Sources/StreamSync/Requests/IDs.swift
@@ -14,5 +14,51 @@ public struct CheckIDsRequest : StreamRequest {
     
     public let IDs          : [String]
     
-    // Response [String]
+    // Response [String] of non existing ids
+}
+
+public struct UpStreamEvent : Codable {
+    public var id             : String
+    public let author          : String
+    public let version         : Int
+    public let createdAt       : Date
+
+    public let content         : Data
+    
+    public init(id: String, author: String, version: Int, createdAt: Date, content: Data) {
+        self.id = id
+        self.author = author
+        self.version = version
+        self.createdAt = createdAt
+        self.content = content
+    }
+}
+
+public struct StreamEvent : Codable {
+    public var _id             : String
+    public let author          : String
+    public let version         : Int
+    public let createdAt       : Date
+    public let receivedAt      : Date
+    public let address         : String?
+    
+    public let content         : Data
+    
+    public init(upStreamEvent up: UpStreamEvent2, address: String?) {
+        self._id = up._id
+        self.author = up.author
+        self.content = up.content
+        self.version = up.version
+        self.receivedAt = Date()
+        self.createdAt = up.createdAt
+        self.address = address
+    }
+    
+//    public init(author: String, content: Data, version: Int, createdAt: Date) {
+//        self.author = author
+//        self.content = content
+//        self.version = version
+//        self.receivedAt = Date()
+//        self.createdAt = createdAt
+//    }
 }
